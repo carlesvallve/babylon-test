26503877e77887600e06bb5bb74433959efb58ce
Carles Vallve <snaptothegrid@gmail.com>
2019-02-28 00:09:27 +0900

    Updated webpack.config with entry and output

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,17 @@ import { Scene } from "@babylonjs/core/scene";
 import { Vector3, Color3 } from "@babylonjs/core/Maths/math";
 import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
 import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { DirectionalLight } from "@babylonjs/core/Lights/directionalLight";
 import { Mesh } from "@babylonjs/core/Meshes/mesh";
-
-import { GridMaterial } from "@babylonjs/materials/grid";
-
-import "@babylonjs/core/Debug/debugLayer"; // Augments the scene with the debug methods
-import "@babylonjs/inspector"; // Injects a local ES6 version of the inspector to prevent automatically relying on the none compatible version
-
-
+// import { GridMaterial } from "@babylonjs/materials/grid";
+import { SimpleMaterial } from "@babylonjs/materials/simple";
 
 // Required side effects to populate the Create methods on the mesh class. Without this, the bundle would be smaller but the createXXX methods from mesh would not be accessible.
 import "@babylonjs/core/Meshes/meshBuilder";
-import { DirectionalLight } from "@babylonjs/core/Lights/directionalLight";
-import { Vector3LineComponent } from "@babylonjs/inspector/components/actionTabs/lines/vector3LineComponent";
+
+// import "@babylonjs/core/Debug/debugLayer"; // Augments the scene with the debug methods
+// import "@babylonjs/inspector"; // Injects a local ES6 version of the inspector to prevent automatically relying on the none compatible version
+
 
 // Get the canvas element from the DOM.
 const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
@@ -25,7 +23,7 @@ const engine = new Engine(canvas);
 
 // Create our first scene.
 var scene = new Scene(engine);
-scene.debugLayer.show();
+// scene.debugLayer.show();
 
 // This creates and positions a free camera (non-mesh)
 var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
@@ -45,8 +43,11 @@ hlight.intensity = 0.2;
 var dlight = new DirectionalLight('dlight', new Vector3(-1, -1, 1), scene)
 dlight.intensity = 0.2;
 
+var material = new SimpleMaterial("material", scene);
+material.diffuseColor = new Color3(0.5, 0.5, 0.5);
+
 // Create a grid material
-var material = new GridMaterial("grid", scene);
+// var material = new GridMaterial("grid", scene);
 // material.mainColor = new Color3(0.5, 0.5, 0.5);
 
 // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
@@ -56,13 +57,13 @@ var sphere = Mesh.CreateSphere("sphere1", 16, 2, scene);
 sphere.position.y = 2;
 
 // Affect a material
-//sphere.material = material;
+sphere.material = material;
 
 // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
 var ground = Mesh.CreateGround("ground1", 6, 6, 2, scene);
 
 // Affect a material
-// ground.material = material;
+ground.material = material;
 
 // Render every frame
 engine.runRenderLoop(() => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   resolve: {
       extensions: ['.ts', '.js']
@@ -7,5 +9,10 @@ module.exports = {
           test: /\.tsx?$/,
           loader: 'ts-loader'
       }]
-  }
+  },
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build/')
+  },
 }
